cdc1f7914748a8c4035d6e8aaf0563692092702e
Lucas Christian <lucaschristian@gmail.com>
2025-06-20 21:48:20 -0300

    ajustar build

--- src/app/comparacao/page.tsx
+++ src/app/comparacao/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import { useAuth } from '@/hooks/useAuth';
-import { useComparison } from '@/hooks/useComparison';
-import { Header } from '@/components/Header';
+import { useAuth } from '../../hooks/useAuth';
+import { useComparison } from '../../hooks/useComparison';
+import { Header } from '../../components/Header';
 import { 
   UserIcon, 
   ChartBarIcon, 
